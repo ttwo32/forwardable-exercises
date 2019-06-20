c16d2f5306f6706149f01fec11bfddfd699b1aef
ttw <ttwo32@hotmail.com>
2019-06-21 01:48:01 +0900

    make my forwadable.

---
+++ reimplement_forwardable/my_forwardable.rb
@@ -0,0 +1,15 @@
+module MyForwardable
+  def def_delegator(accessor, method, ali = method)
+    module_eval <<-RUBY
+    def #{ali}(*args)
+      #{accessor}.#{method}(*args)
+    end
+    RUBY
+    
+  end
+  
+  def def_delegators(accessor, *methods)
+    methods.each{|m|def_delegator(accessor, m)}
+  end
+end
+
